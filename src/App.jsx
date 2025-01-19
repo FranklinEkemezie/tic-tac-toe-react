98cc7468c498f0edba8e0d9df1a983da2603a61f
Shadow Walker <franklynpeter2006@gmail.com>
2025-01-20 00:42:31 +0100

    update parent commponents - App, Main to include new child components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import './App.css'
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import GameContextProvider from "./contexts/gameContextProvider.jsx";
+import GameControlCentre from './components/GameControlCentre.jsx';
 
 function App() {
-
     return (
         <div className="App">
-            <Header />
-            <Main />
-            <Footer />
+            <GameContextProvider>
+                <GameControlCentre>
+                    <Header />
+                    <Main />
+                    <Footer />
+                </GameControlCentre>
+            </GameContextProvider>
         </div>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,18 @@
 import Board from "./Board.jsx";
 import Timer from "./Timer.jsx";
+import GameOverModal from "./GameOverModal.jsx";
+import {useContext} from "react";
+import GameContext from "../contexts/gameContext.jsx";
 
 function Main() {
+
+    const { board } = useContext(GameContext);
 
     return (
         <div>
             <Timer />
-            <Board />
+            <Board board={board} />
+            <GameOverModal />
         </div>
     );
 }

71fce8b7ae6b747e150621008c6828115606d284
Shadow Walker <franklynpeter2006@gmail.com>
2025-01-20 00:24:33 +0100

    add functionality for footer restart button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,44 @@
 import footerStyles from '../assets/styles/footer.module.css';
+import {useContext} from "react";
+import GameContext from "../contexts/gameContext.jsx";
+import useGame from '../hooks/useGame.js';
 
 function Footer() {
+
+    const { currentState, board, nextPlayer, players, restartGame } = useContext(GameContext);
+    const { suggestMove, getMinmaxMove } = useGame();
+
+    function handleSuggestMove() {
+
+        if (currentState !== 'ON') {
+            alert('Game not started');
+            return;
+        }
+
+        const suggestedMove = suggestMove(board, () => getMinmaxMove(
+            board, nextPlayer, players
+        ));
+
+        alert(`Suggested box number: ${suggestedMove + 1}`);
+    }
 
     return (
         <div className={footerStyles.footer}>
             <div>
-                <button className={footerStyles.restartBtn}>Restart Game</button>
+                <button
+                    className={footerStyles.restartBtn}
+                    onClick={restartGame}
+                >
+                    Restart Game
+                </button>
             </div>
             <div>
-                <button className={footerStyles.suggestBtn}>Suggest Move</button>
+                <button
+                    className={footerStyles.suggestBtn}
+                    onClick={handleSuggestMove}
+                >
+                    Suggest Move
+                </button>
 
             </div>
         </div>
